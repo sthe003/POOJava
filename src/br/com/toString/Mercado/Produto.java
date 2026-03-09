31fb7709d3678ca340aaf2612f0f91cf7f7d331c
Sthefanie <sthefaniefernanda19@gmail.com>
2026-03-09 00:33:44 -0300

    Implementando ArrayList

--- src/br/com/toString/Mercado/Produto.java
+++ src/br/com/toString/Mercado/Produto.java
@@ -1,9 +1,17 @@
 package br.com.toString.Mercado;
 
+import java.util.ArrayList;
+
 public class Produto {
     private String nome;
     private double preco;
     private int quantidade;
+
+    public Produto(int quantidade, double preco, String nome) {
+        this.quantidade = quantidade;
+        this.preco = preco;
+        this.nome = nome;
+    }
 
     public String getNome() {
         return nome;
@@ -29,8 +37,31 @@ public class Produto {
         this.quantidade = quantidade;
     }
 
+    public static void main(String[] args) {
+        ArrayList<Produto>notaFiscal = new ArrayList<>();
+
+        Produto produto1 = new Produto(5, 15.99, "Sabão");
+        Produto produto2 = new Produto(2, 10.99, "Detergente");
+
+
+        notaFiscal.add(produto1);
+        notaFiscal.add(produto2);
+
+        System.out.println("Itens: " + notaFiscal.size());
+        System.out.println("Produto na posição 0: " + notaFiscal.get(0).getNome());
+
+        Produto produtoNovo = new Produto(3, 15.99, "novo produto");
+        System.out.println(produtoNovo);
+
+        for (Produto produto : notaFiscal){
+            System.out.println(produto);
+
+        }
+
+    }
+
     @Override
     public String toString(){
-        return "Produtos: " + this.getNome() + this.getPreco() * this.getQuantidade();
+        return "Nome " + nome + " Preço: " + preco + " Quantidade: "+ quantidade;
     }
 }
--- src/br/com/toString/Mercado/ProdutoPerecivel.java
+++ src/br/com/toString/Mercado/ProdutoPerecivel.java
@@ -3,4 +3,13 @@ package br.com.toString.Mercado;
 public class ProdutoPerecivel extends Produto{
     private String dataValidade;
 
+    public ProdutoPerecivel(int quantidade, double preco, String nome, String dataValidade) {
+        super(quantidade, preco, nome);
+        this.dataValidade = dataValidade;
+    }
+
+    public static void main(String[] args) {
+        ProdutoPerecivel produtoPerecivel = new ProdutoPerecivel(4, 12.99, "Produto C", "2026-03-09");
+        System.out.println(produtoPerecivel);
+    }
 }
