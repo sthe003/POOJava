1ed316c5625bebd064f812878930df541ed2a87d
Sthefanie <sthefaniefernanda19@gmail.com>
2026-03-15 01:29:25 -0300

    Implementando ArrayList

--- src/br/com/toString/Compras/Lista.java
+++ src/br/com/toString/Compras/Lista.java
@@ -4,13 +4,10 @@ import java.util.ArrayList;
 
 public class Lista{
 
-
-
     public static void main(String[] args) {
         Produto primeiroProduto = new Produto("Arroz", 15.99, 3);
         Produto segundoProduto = new Produto("Feijão", 19.99, 2);
         Produto terceiroProduto = new Produto("Ovo", 9.99, 1);
-
 
         ArrayList<Produto>listaDeProdutos = new ArrayList<>();
         listaDeProdutos.add(primeiroProduto);
