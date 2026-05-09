d9fe13e941be191e26009f393862404806c43ea1
Sthefanie <sthefaniefernanda19@gmail.com>
2026-05-09 01:18:01 -0300

    Projeto banco

---
+++ src/projetos/banco/Principal.java
@@ -0,0 +1,10 @@
+package projetos.banco;
+
+import java.util.Scanner;
+import java.util.SortedMap;
+
+public class Principal {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+
+}
