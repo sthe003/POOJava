750b768c0c6c331c11d5592f54193db62a34e49a
Sthefanie <sthefaniefernanda19@gmail.com>
2026-05-29 00:41:48 -0300

    Consumindo API Google Books

---
+++ src/projetos/googleBooksAPI/buscandoLivro.java
@@ -0,0 +1,32 @@
+package projetos.googleBooksAPI;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Scanner;
+
+public class buscandoLivro {
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        Scanner leitura = new Scanner(System.in);
+        System.out.println("Qual livro você procura? ");
+        var busca = leitura.nextLine().replace(" ", "+");
+
+        //escondendo a Key
+        String apiKey = System.getenv("GOOGLE_BOOKS_KEY");
+        String endereco = "https://www.googleapis.com/books/v1/volumes?q="
+                + busca
+                + "&key=" + apiKey;
+
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(endereco))
+                .build();
+        HttpResponse<String> response = client
+                .send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println(response.body());
+    }
+}
