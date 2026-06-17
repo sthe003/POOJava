b147ad416ccfff62530ba4c41773184281ccf74f
Sthefanie <sthefaniefernanda19@gmail.com>
2026-06-17 19:40:08 -0300

    Consumindo API ViaCEP e manipulando JSON - melhorando estrutura de classes

--- src/br/com/consumindoAPIs/projetoViaCEPAPI/Principal.java
+++ src/br/com/consumindoAPIs/projetoViaCEPAPI/Principal.java
@@ -2,14 +2,7 @@ package br.com.consumindoAPIs.projetoViaCEPAPI;
 
 //https://viacep.com.br/ws/01001000/json/ - link api
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-
 import java.io.IOException;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
 import java.util.Scanner;
 
 public class Principal {
